8a56cad76c74ef3f1f206b1439200221915efc17
Pandeul <pandeul@MacBook-Pro-de-Pandeul.local>
2017-11-17 09:56:39 +0100

    ajout du MainTest pour les TU

--- src/Tests/MainTests.java
+++ src/Tests/MainTests.java
@@ -1,5 +1,14 @@
 package Tests;
 
-public class MainTests {
+import org.junit.*;
+import static org.junit.Assert.*;
 
+public class MainTests {
+	
+	@Test
+	public void testThis() throws Exception
+	{
+		assertEquals(new Long(3), new Long(3));
+	}
+	
 }
