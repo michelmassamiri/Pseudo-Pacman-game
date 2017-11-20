560c1e1bfb24fcebe66b0708d86b5b9cc8170a88
Pandeul <pandeul@MacBook-Pro-de-Pandeul.local>
2017-11-20 15:18:51 +0100

    modification du gitignore et ajout d'un premier commentaire pour documentation dans le controlleur

--- src/Controleur/Controleur.java
+++ src/Controleur/Controleur.java
@@ -8,6 +8,11 @@ public class Controleur {
 	private Model model;
 	private Vue vue;
 	
+	/**
+	 * Constructeur du Controleur permettant la gestion des evennements dans le programme.
+	 * @param m Model du design pattern MVC
+	 * @param v Vue du design pattern MVC
+	 */
 	public Controleur(Model m, Vue v)
 	{
 		model = m;
