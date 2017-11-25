e56ae897a8be58971a151248fff854f7391dce94
enzperuzzetto <enzo.peruzzetto@etu.u-bordeaux.fr>
2017-11-25 14:20:58 +0100

    Changement de place des images et affiche une image dans le pane

--- src/View/ViewFrame.java
+++ src/View/ViewFrame.java
@@ -4,9 +4,10 @@ import java.awt.Frame;
 
 import Controller.GameController;
 
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
@@ -18,8 +19,7 @@ public class ViewFrame {
 	static final int CELL = 9; //size of the cells (in units)
 	public static final Paint WALLCOLOR = Color.BURLYWOOD;
 	
-	private static GridPane pane;
-	private ImageView imageView;
+	private static Pane pane;
 	
 	public ViewFrame()
 	{
@@ -28,7 +28,8 @@ public class ViewFrame {
 	
 	public void start(Stage primaryStage){
 		primaryStage.setTitle("Pseudo PAC-MAN");
-		//drawFrame(primaryStage, nbx, nby);
+		drawFrame(primaryStage, 10, 10);
+		drawObject("player.png");
 		primaryStage.show();
 	}
 	
@@ -40,6 +41,14 @@ public class ViewFrame {
 	 */
 	public static void drawFrame(Stage stage, int nbrX, int nbrY){
 		//TODO by jeniffer
+		pane = new Pane();
+		Scene scene = new Scene(pane, 
+						((WALL + CELL) * nbrX + WALL)* SPAN, 
+						((WALL + CELL) * nbrY + WALL)* SPAN);
+		scene.setFill(Color.WHITE);
+		
+		stage.setScene(scene);
+		
 	}
 	
 	
@@ -79,13 +88,13 @@ public class ViewFrame {
 	 */
 	private void drawObject(String nameJPG){
 		Image image = new Image( getClass().getResource(nameJPG).toExternalForm());
-		imageView = new ImageView(image);
-		/*Frame.pane.getChildren().add(this.imageView);
+		ImageView sprite= new ImageView(image);
+		pane.getChildren().add(sprite);
 		double xt = (int) ((ViewFrame.WALL + sprite.getX() * (ViewFrame.WALL + ViewFrame.CELL)) * ViewFrame.SPAN);
 		double yt = (int) ((ViewFrame.WALL + sprite.getY() * (ViewFrame.WALL + ViewFrame.CELL)) * ViewFrame.SPAN);
 		
-		imageView.setX(xt);
-		imageView.setY(yt);*/
+		sprite.setX(xt);
+		sprite.setY(yt);
 	}
 	
 }
