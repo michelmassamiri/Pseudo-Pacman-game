d772bfa428d3dcd56221d40e8d453083ebdc301a
Pandeul <pandeul@MacBook-Pro-de-Pandeul.local>
2017-11-28 14:09:08 +0100

    ajout d'une fonction permettan de definir a quel endroit se situe l'entite joueur dans la table des entites

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -7,19 +7,27 @@ import java.util.Vector;
 public class Model {
 
 	private Vector<Entity> entities;
+	private int playerRank;
 
 	public Model()
 	{
-		
+		playerRank = 0;
 	}
 
 	/**
 	 * Method that makes simpler the drawing of each entities. By calling this method and iterate through the vector,
      * you can then draw them with the getImage method from the Entity interface.
+	 * The first element should always be the player. If not, it has to be precised.
      * @see Entity
 	 * @return a vector with all the entities of the current game.
 	 */
     public Vector<Entity> getEntities() {
         return entities;
     }
+
+    /**
+     * set the rank of the player in the entities table. Default rank is zero.
+     * @param rank the rank in the entities table.
+     */
+    public void setPlayerRank(int rank) { playerRank = rank; }
 }
