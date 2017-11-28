52a091e9feab9899b2dfdc7b007b012a56f96c8f
Pandeul <pandeul@MacBook-Pro-de-Pandeul.local>
2017-11-28 13:55:31 +0100

    legeres modifications pour ajouter de la doc

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -13,6 +13,12 @@ public class Model {
 		
 	}
 
+	/**
+	 * Method that makes simpler the drawing of each entities. By calling this method and iterate through the vector,
+     * you can then draw them with the getImage method from the Entity interface.
+     * @see Entity
+	 * @return a vector with all the entities of the current game.
+	 */
     public Vector<Entity> getEntities() {
         return entities;
     }
