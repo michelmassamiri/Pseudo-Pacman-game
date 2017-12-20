47a3f0a3bf1864fb1a3b772ca4160d50e1d4ba97
Jeniffer <jeniffer.ortiz-lozano@etu.u-bordeaux.fr>
2017-12-21 00:31:34 +0100

    Bad Guys Controller

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -26,16 +26,16 @@ import javafx.util.Duration;
 public class GameController {
     private static GameController instance = null;
     private PlayerController playerController;
+    private BadGuyController badGuyController;
     private Model model;
     private ViewFrame viewFrame;
     private Vector<StaticEntity> candyList;
     private StaticEntity buttonOpen, buttonClose, door;
     private Timeline timeline;
-    private Vector<BadGuy> badGuys;
 
 
     private int score;
-    private boolean gameOver;
+    private boolean GameOver;
     private boolean win;
 
     /**
@@ -55,7 +55,6 @@ public class GameController {
     private GameController() {
         model = new Model();
         viewFrame = ViewFrame.getInstance();
-        playerController = new PlayerController();
         model.loadAll();
         //Edge wall = model.getLabyrinth().closeDoorRandom();
         Vertex vertex = model.getLabyrinth().randomVertexByDir();
@@ -65,11 +64,9 @@ public class GameController {
             wall = model.getLabyrinth().getEdge(vertex, Directions.NORTH);
         }
         model.getLabyrinth().closeDoor(wall);
-        //player = Player.getInstance();
-
-        badGuys = new Vector<>();
-        badGuys.add(new BadGuy(1, 2));
-        badGuys.add(new BadGuy(3, 6));
+        playerController = new PlayerController();
+        badGuyController = new BadGuyController();
+        
 
         candyList = new Vector<StaticEntity>();
         candyList.add(new StaticEntity(Resources.CANDY_1, 2, 1, new StaticCandyAction(10)));
@@ -88,33 +85,32 @@ public class GameController {
         model.addEntity(buttonOpen);
         model.addEntity(buttonClose);
         model.addEntity(door);
-        for (BadGuy b : badGuys)
-            model.addEntity(b);
-
+        
+        model.addEntity(badGuyController.getBadGuys()[0]);
+        model.addEntity(badGuyController.getBadGuys()[1]);
+        
         timeline = new Timeline(new KeyFrame(
-                Duration.millis(1400),
-                eventMoveBadGuy));
+        		Duration.millis(1000),
+        		eventMoveBadGuy));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
-
+        
     }
-
+    
+    
     /**
      *
      */
     public EventHandler<ActionEvent> eventMoveBadGuy = new EventHandler<ActionEvent>() {
 
         public void handle(ActionEvent event) {
-            for (BadGuy b : badGuys)
-                b.Manhatan(model.getLabyrinth());
-            for (BadGuy b : badGuys) {
-                if (b.getAction().isStartable()) {
-                    gameOver = true;
-                    timeline.stop();
-                    viewFrame.gameOver(score);
-                }
-            }
+        	badGuyController.move();
+        	if (badGuyController.getGameStatus()== true){
+        		timeline.stop();
+                viewFrame.gameOver(score);
+        	}
         }
+                
     };
 
     /**
@@ -123,6 +119,12 @@ public class GameController {
     public Vector<Entity> getEntities() {
         return model.getEntities();
     }    
+    
+    public Model getModel() {
+		
+		return model;
+	}
+
     
     /**
      * Necessary for the view in order to draw the Labyrinth
@@ -138,7 +140,8 @@ public class GameController {
     public void setScore(int value) {
         score = score + value;
     }
-
+    
+   
     /**
      * @return
      */
@@ -150,14 +153,14 @@ public class GameController {
      * @param value
      */
     public void setGameOver(boolean value) {
-        gameOver = value;
+    	GameOver = value;
     }
 
     /**
      * @return
      */
     public boolean getGameOver() {
-        return gameOver;
+        return GameOver;
     }
 
     /**
@@ -195,8 +198,7 @@ public class GameController {
             KeyCode keycode = event.getCode();
             if (Player.getInstance().getDirection(keycode) != null) {
             	playerController.moveChange(keycode);
-
-                    for (StaticEntity candy : candyList) {
+            	for (StaticEntity candy : candyList) {
                         if (candy.getAction().isStartable()) {
                             candy.getAction().actions();
                             candy.setPosX(-1);
@@ -223,14 +225,11 @@ public class GameController {
                         System.out.println("close");
                     }
 
-                    for (BadGuy b : badGuys) {
-                        if (b.getAction().isStartable()) {
-                            gameOver = true;
-                            timeline.stop();
-                            viewFrame.gameOver(score);
-                        }
+                    if (badGuyController.getGameStatus()== true) {
+    				timeline.stop();
+    				viewFrame.gameOver(score);
                     }
-
+                   
                 }
                 if (keycode == KeyCode.ESCAPE)
                     System.exit(0);
